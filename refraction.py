492a2578f089edcef8691803a76b46efbb88e131
schornakj <joe.schornak@gmail.com>
2017-10-06 16:16:00 -0400

    Rotate ray by an angle in plane containing ray and face normal

--- refraction.py
+++ refraction.py
@@ -1,12 +1,13 @@
 import trimesh
 import numpy as np
+import pyquaternion
 
 transform = np.eye(4)
 transform[0,3]=1
 mesh = trimesh.primitives.Box(extents=np.array([1,2,3]), transform=transform)
 
 ray_origin = np.array([[0,0,0]])
-ray_direction = np.array([[1,0,0]])
+ray_direction = np.array([[1,0.1,0]])
 
 ret = mesh.ray.intersects_location(ray_origin, ray_direction)
 triangles, rays, locations = mesh.ray.intersects_id(ray_origin, ray_direction, return_locations=True)
@@ -20,5 +21,16 @@ intersections_sorted = sorted(zip(distances, triangles, locations), key=lambda x
 
 triangle_nearest = intersections_sorted[0][1]
 location_nearest = intersections_sorted[0][2]
+normal_nearest = mesh.face_normals[triangle_nearest]
 
-print("Location " +str(location_nearest) + " normal direction " + str(mesh.face_normals[triangle_nearest]))
+print("Location " +str(location_nearest) + " normal direction " + str(normal_nearest))
+
+axis_rotation = np.cross(ray_direction, normal_nearest)
+axis_rotation_norm = axis_rotation/np.linalg.norm(axis_rotation)
+print("Rotation axis: " + str(axis_rotation_norm))
+
+# rotation_euler = trimesh.transformations.euler_from_quaternion(trimesh.transformations.quaternion_about_axis(np.pi/4, axis_rotation_norm.T))
+rotation_mat = trimesh.transformations.rotation_matrix(np.pi/4, axis_rotation_norm.T)
+print(rotation_mat)
+print("Original direction: " + str(ray_direction) + " Rotated direction: " + str(ray_direction * rotation_mat[0:3,0:3]))
+print(np.linalg.norm(ray_direction * rotation_mat[0:3,0:3]))
